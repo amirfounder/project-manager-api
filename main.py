6359b26f530ea6bd1bbdaa3c35966270d87f07c9
Amir Sharapov <amir@amirfounder.com>
2022-01-14 00:33:13 -0600

    enabled cors

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
+from flask_cors import CORS
 from src.controllers import *
 from src.data import setup_schema
 from src.utils import *
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route(FLASK_PROJECTS_ID_ENDPOINT, methods=['GET', 'PUT', 'DELETE'])
